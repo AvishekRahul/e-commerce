d99ea7c2fcaad7b7df7e0b7cc1444cbd3167be16
avishekpatra <avishek.patra1@outlook.com>
2024-05-27 14:50:54 +0530

    this is login and signup page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='bg-slate-200'>
+      <div className='container mx-auto p-4'>
+      <p className='text-center font-bold'>Coding With Avishek</p>
+      </div>
+     
+    </footer>
   )
 }
 
